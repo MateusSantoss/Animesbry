b0de68e363db4a7880590152c51f09c400312822
Mateus santos <msjt098@gmail.com>
2023-05-24 10:24:40 -0300

    Ajustes realizados, agr o icone de tema escuro só aparece se o tema estiver claro, e vice versa

--- global.js
+++ global.js
@@ -1,23 +1,36 @@
+//Por padrão o tema é claro
 let temaPagina = 'claro'
-
+//\\\\a função é chamada nos icones na pagina inicial
+//os parametros são string, claro ou escuro
 function mudarTema (temaPagina) {
 			
 
 			
 
-			 
+			 //A pagina index.html é recarregado concatenada com o tema escolhido
 			 window.location.href='index.html?' + temaPagina
 
 		
 		}
-
+        //Função chamada no onload da página
 		function verificarTema () {
+			let temaEscuro = false
+			//variavel tema recuperar o tema na url
 			let tema = window.location.search
+			//substitui o "?" por um espaço vazio
 			tema = tema.replace('?', '')
 			console.log(tema)
+			//se o tema na url for escuro
 			if (tema === 'escuro') {
-               document.getElementById('conteudo').className = 'tema'
+               
+                temaEscuro = true
+               
 			} 
+			if (temaEscuro) {
+				document.getElementById('conteudo').className = 'tema'
+			}
+
+
 
 		}
        
@@ -28,9 +41,20 @@ function mudarTema (temaPagina) {
 			console.log(tema)
         	 window.location.href=pagina + '?' + tema
         }
-
         
 
+       function paginaIncial () {
+        	let tema = window.location.search
+			tema = tema.replace('?', '')
+
+			
+			window.location.href = "index.html?" + tema
+		
+				
+
+               }        
+
+        
 
 
 		
